0b0f097bd08eeca84e84c3c689755c7d3e312044
WHD597312 <1352250152@qq.com>
2020-07-15 19:55:31 +0800

    增加蓝牙电子秤称重功能

---
+++ module_wom_ble/src/main/java/com/supcon/mes/module_wom_ble/model/listener/OnSuccessListener.java
@@ -0,0 +1,11 @@
+package com.supcon.mes.module_wom_ble.model.listener;
+
+/**
+ * Created by wangshizhan on 2018/8/2
+ * Email:wangshizhan@supcom.com
+ */
+public interface OnSuccessListener<T> {
+
+    void onSuccess(T result);
+
+}
