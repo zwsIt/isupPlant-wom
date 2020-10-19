08b03b73dea8f5284daed4d68c653c3c34de47ba
zhangwenshuai <zhangwenshuai1@supcon.com>
2020-10-19 20:45:35 +0800

    master：投料活动：尾料、尾料参照

---
+++ module_wom_producetask/src/main/java/com/supcon/mes/module_wom_producetask/model/bean/RemainMaterialEntity.java
@@ -0,0 +1,12 @@
+package com.supcon.mes.module_wom_producetask.model.bean;
+
+import com.supcon.common.com_http.BaseEntity;
+
+/**
+ * ClassName
+ * Created by zhangwenshuai1 on 2020/10/15
+ * Email zhangwenshuai1@supcon.com
+ * Desc
+ */
+public class RemainMaterialEntity extends BaseEntity {
+}
