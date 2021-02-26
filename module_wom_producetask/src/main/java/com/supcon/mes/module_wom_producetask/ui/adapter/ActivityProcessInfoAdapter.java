c22c7e6cbd3daa5fa59a17832db513747d3d0a83
zhangwenshuai <zhangwenshuai1@supcon.com>
2021-02-26 11:15:46 +0800

    优化活动执行流程

--- module_wom_producetask/src/main/java/com/supcon/mes/module_wom_producetask/ui/adapter/ActivityProcessInfoAdapter.java
+++ module_wom_producetask/src/main/java/com/supcon/mes/module_wom_producetask/ui/adapter/ActivityProcessInfoAdapter.java
@@ -3,6 +3,7 @@ package com.supcon.mes.module_wom_producetask.ui.adapter;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import com.app.annotation.BindByTag;
 import com.supcon.common.view.base.adapter.BaseListDataRecyclerViewAdapter;
 import com.supcon.common.view.base.adapter.viewholder.BaseRecyclerViewHolder;
+import com.supcon.common.view.util.DisplayUtil;
 import com.supcon.mes.mbap.utils.DateUtil;
 import com.supcon.mes.middleware.constant.Constant;
 import com.supcon.mes.module_wom_producetask.R;
@@ -21,7 +23,7 @@ import com.supcon.mes.module_wom_producetask.constant.WomConstant;
 import com.supcon.mes.module_wom_producetask.model.bean.TaskActiveEntity;
 
 /**
- *
+ * zws  活动执行流程图
  */
 public class ActivityProcessInfoAdapter extends BaseListDataRecyclerViewAdapter<TaskActiveEntity> {
 
@@ -36,9 +38,10 @@ public class ActivityProcessInfoAdapter extends BaseListDataRecyclerViewAdapter<
 
     class ViewHolder extends BaseRecyclerViewHolder<TaskActiveEntity> {
 
-
         @BindByTag("index")
         TextView index;
+        @BindByTag("itemAreaLineTop")
+        View itemAreaLineTop;
         @BindByTag("itemAreaDot")
         ImageView itemAreaDot;
         @BindByTag("itemAreaLineBottom")
@@ -51,6 +54,8 @@ public class ActivityProcessInfoAdapter extends BaseListDataRecyclerViewAdapter<
         TextView material;
         @BindByTag("dealTime")
         TextView dealTime;
+        @BindByTag("statusTv")
+        TextView statusTv;
 
         public ViewHolder(Context context) {
             super(context, parent);
@@ -69,26 +74,55 @@ public class ActivityProcessInfoAdapter extends BaseListDataRecyclerViewAdapter<
         @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override
         protected void update(TaskActiveEntity data) {
-//            if (getAdapterPosition() == getListSize() - 1) {
-//                itemAreaLineBottom.setVisibility(View.INVISIBLE);
-//            }
+
+            if (getAdapterPosition() == 0){
+                RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) itemView.getLayoutParams();
+                layoutParams.topMargin = DisplayUtil.dip2px(10,context);
+                itemView.setLayoutParams(layoutParams);
+            }
+
             index.setText(data.getExecSort());
             activityName.setText(String.format("%s：%s", data.getActiveType().value, data.getName()));
-            if (WomConstant.SystemCode.EXE_STATE_END.equals(data.getRunState().id)){
-                index.setTextColor(context.getResources().getColor(R.color.gray_text));
-                activityName.setTextColor(context.getResources().getColor(R.color.gray_text));
-                itemAreaDot.setImageResource(R.drawable.ic_point_end);
-                dealTime.setText(DateUtil.dateFormat(data.getActEndTime(), "MM-dd HH:mm"));
-            }else if (WomConstant.SystemCode.EXE_STATE_ING.equals(data.getRunState().id)){
-                index.setTextColor(context.getResources().getColor(R.color.wom_tv_status_color));
-                activityName.setTextColor(context.getResources().getColor(R.color.wom_tv_status_color));
-                itemAreaDot.setImageResource(R.drawable.ic_point_ing);
-                dealTime.setText(DateUtil.dateFormat(data.getActStartTime(), "MM-dd HH:mm"));
+            statusTv.setText(data.getRunState().value);
+
+            if (getAdapterPosition() == 0){
+                itemAreaLineTop.setBackgroundColor(context.getResources().getColor(R.color.white));
             }else {
-                index.setTextColor(context.getResources().getColor(R.color.black));
-                activityName.setTextColor(context.getResources().getColor(R.color.black));
-                itemAreaDot.setImageResource(R.drawable.ic_point);
+                itemAreaLineTop.setBackgroundColor(context.getResources().getColor(R.color.wom_blue_5DB7FF));
+            }
+
+            if (WomConstant.SystemCode.EXE_STATE_END.equals(data.getRunState().id)){
+                activityName.setTextColor(context.getResources().getColor(R.color.wom_black_333333));
+                itemAreaDot.setImageResource(R.drawable.wom_ic_node_blue);
+                itemAreaLineBottom.setBackgroundColor(context.getResources().getColor(R.color.wom_blue_5DB7FF));
+                dealTime.setText(DateUtil.dateFormat(data.getActEndTime(), "MM-dd HH:mm"));
+                statusTv.setTextColor(context.getResources().getColor(R.color.dark_green));
+            }else if (WomConstant.SystemCode.EXE_STATE_ING.equals(data.getRunState().id)){
+                activityName.setTextColor(context.getResources().getColor(R.color.wom_blue_5DB7FF));
+                itemAreaDot.setImageResource(R.drawable.wom_ic_node_blue);
+                itemAreaLineBottom.setBackgroundColor(context.getResources().getColor(R.color.wom_blue_5DB7FF));
+                dealTime.setText(DateUtil.dateFormat(data.getActStartTime(), "MM-dd HH:mm"));
+                statusTv.setTextColor(context.getResources().getColor(R.color.wom_blue_5DB7FF));
+            }else {
+                activityName.setTextColor(context.getResources().getColor(R.color.gray_text));
+                itemAreaDot.setImageResource(R.drawable.wom_ic_node_gery);
+                itemAreaLineBottom.setBackgroundColor(context.getResources().getColor(R.color.wom_grey_DDDDDD));
                 dealTime.setText("--");
+                statusTv.setTextColor(context.getResources().getColor(R.color.gray_text));
+
+                if (getItem(getAdapterPosition() -1) != null && !WomConstant.SystemCode.EXE_STATE_WAIT.equals(getItem(getAdapterPosition() -1).getRunState().id)){
+                    itemAreaLineTop.setBackgroundColor(context.getResources().getColor(R.color.wom_blue_5DB7FF));
+                }else {
+                    if (getAdapterPosition() != 0){
+                        itemAreaLineTop.setBackgroundColor(context.getResources().getColor(R.color.wom_grey_DDDDDD));
+                    }
+                }
+            }
+
+            if (getAdapterPosition() == getItemCount() -1 ){
+                itemAreaLineBottom.setVisibility(View.GONE);
+            }else {
+                itemAreaLineBottom.setVisibility(View.VISIBLE);
             }
 
             if (data.getMaterialId() != null && !TextUtils.isEmpty(data.getMaterialId().getCode())){
@@ -96,17 +130,6 @@ public class ActivityProcessInfoAdapter extends BaseListDataRecyclerViewAdapter<
             }else {
                 material.setText("--");
             }
-
-//            SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder("进行了"+data.dealStaff +"操作，处理结果" + data.operateDes);
-//            String html = "进行了<font color='#366CBC'>"+data.activityName+"</font>操作，处理结果<font color='#366CBC'>" + data.operateDes + "</font>";
-//            activityName.setText(Html.fromHtml(html));
-//            if (TextUtils.isEmpty(data.dealAdvice)){
-//                dealAdvice.setVisibility(View.GONE);
-//            }else {
-//                dealAdvice.setVisibility(View.VISIBLE);
-//                dealAdvice.setText(String.format("意见：%s", data.dealAdvice));
-//            }
-//            dealTime.setText(DateUtil.dateFormat(data.dealTime, Constant.TimeString.YEAR_MONTH_DAY_HOUR_MIN_SEC));
         }
     }
 }
